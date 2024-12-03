f27b8f3f2f14bff307b51096720a75e7ed4ccec2
VishnuAravind12 <vishnuaa77@gmail.com>
2024-12-03 16:01:27 -0600

    card strcutrue

---
+++ src/cards.rs
@@ -0,0 +1,34 @@
+// src/cards.rs
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub enum Rank {
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
